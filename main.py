813174647af3953bcd2063f58f6ed13523606c07
Victor Augusto <victor.silva452@outlook.com>
2021-05-17 07:48:07 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,27 @@
+import re
+
+def isAttribuitionClass(statement):
+    re.compile("")
+    return True
+
+def isNumber(statement):
+    return True
+
+################################# Regex #################################
+# \/\/  => (\/\/).*
+# #     => (\#).*
+# /**/  => 
+def isCommentary(statement):
+    hastagComment = re.compile("(\#).*")
+    doubleSlash = re.compile("(\/\/).*")
+    statement.
+    return False
+
+def isVariableIdentifier(statement):
+    return True
+
+
+statement=(input("Entre com o statement: "))
+print ("Statement: ", statement)
+
+
