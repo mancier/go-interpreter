dbb14c76ebe7fccb9f96c5e37836a177421709ba
Victor Augusto <victor.silva452@outlook.com>
2021-05-17 15:09:47 -0300

    adiciona regex em comentario e numeros

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import re
 
+# (^[A-z]\w+)(\:\=)\s
 def isAttribuitionClass(statement):
     re.compile("")
     return True
 
 def isNumber(statement):
+    numberRegex = re.compile("[0-9]")
+
+    if re.match(numberRegex, statement): return True
+
     return True
 
 ################################# Regex #################################
@@ -14,7 +19,12 @@ def isNumber(statement):
 def isCommentary(statement):
     hastagComment = re.compile("(\#).*")
     doubleSlash = re.compile("(\/\/).*")
-    statement.
+    # blockCommentary = re.compile("")
+    
+    if re.match(hastagComment,statement): return True
+    if re.match(doubleSlash,statement): return True
+    # if statement.match(blockCommentary): return True
+    
     return False
 
 def isVariableIdentifier(statement):
