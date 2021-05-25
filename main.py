482d845fdd8ee693f2e5bbd165a3e1a138463664
Victor Augusto <victor.silva452@outlook.com>
2021-05-24 23:40:10 -0300

    :wrench: fix commentary

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 
-def isAttributionClass(statement):
-    varStatement = re.compile("(?<=(^var\s))[A-z]{0,}\w+") # identify var foo
+def isAttribution(statement):
+    varStatement = re.compile("(?<=(^var\s))[A-z]{0,}\w+") # identify var foo  
     defaultStatement = re.compile("([A-z])(?<!\:\=)\w+") # Identify foo := bar
 
     if re.match(defaultStatement, statement): return True
@@ -17,17 +17,15 @@ def isNumber(statement):
     return False
 
 def isCommentary(statement):
-    hastagComment = re.compile("(\#).*")
     doubleSlash = re.compile("(\/\/).*")
     blockCommentary = re.compile("\/\*(\*(?!\/)|[^*])*\*\/")
     
-    if re.match(hastagComment,statement): return True
     if re.match(doubleSlash,statement): return True
     if statement.match(blockCommentary): return True
     
     return False
 
-def isVariableIdentifier(statement):
+def isVariable(statement):
     variableIdentifier = re.compile("^[A-z]\w+")
 
     if re.match(variableIdentifier,statement): return True
@@ -35,8 +33,8 @@ def isVariableIdentifier(statement):
     return False
 
 def checkType(statement): 
-    if isAttributionClass(statement): return "attribution"
-    if isVariableIdentifier(statement): return "variable"
+    if isAttribution(statement): return "attribution"
+    if isVariable(statement): return "variable"
     if isNumber(statement): return "number"
     if isCommentary(statement): return "commentary"
     return "invalid"
