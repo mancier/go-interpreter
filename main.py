8a93fde7fcc9470f0b165c95862b530e14e23c57
Victor Augusto <victor.silva452@outlook.com>
2021-05-25 19:39:20 -0300

    :bug: fix regex

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import re
 
 def isAttribution(statement):
-    varStatement = re.compile("(?<=(^var\s))[A-z]{0,}\w+") # identify var foo  
-    defaultStatement = re.compile("([A-z]{0,})(?<!\:\=)\w+") # Identify foo := bar
+    varStatement = re.compile("((^var\s)|(^const\s))[A-z]{0,}\w+") # identify var foo  
+    defaultStatement = re.compile("([A-z]{0,})(\:\=)\w+") # Identify foo := bar
 
     if re.match(defaultStatement, statement): return True
     if re.match(varStatement, statement): return True
@@ -10,7 +10,7 @@ def isAttribution(statement):
     return False
 
 def isNumber(statement):
-    numberRegex = re.compile("(([0-9]+)(\.|\,[0-9]+)?)|(\.?[0-9]+)\d+") # identify natural/real numbers
+    numberRegex = re.compile("((([0-9]+)(\.|\,[0-9]+)?)|(\.?[0-9]+))\d") # identify natural/real numbers
 
     if re.match(numberRegex, statement): return True
 
@@ -21,12 +21,12 @@ def isCommentary(statement):
     blockCommentary = re.compile("\/\*(\*(?!\/)|[^*])*\*\/")
     
     if re.match(doubleSlash,statement): return True
-    if statement.match(blockCommentary): return True
+    if re.match(blockCommentary, statement): return True
     
     return False
 
 def isVariable(statement):
-    variableIdentifier = re.compile("^[A-z]\w+")
+    variableIdentifier = re.compile("([a-zA-Z])")
 
     if re.match(variableIdentifier,statement): return True
 
@@ -39,6 +39,10 @@ def checkType(statement):
     if isCommentary(statement): return "commentary"
     return "invalid"
 
-statement=(input("Entre com o statement: "))
-typeVariable=checkType(statement)
-print (statement, typeVariable)
+while True:
+    statement=(input("Entre com o statement: "))
+
+    if statement == "exit": break
+    
+    typeVariable=checkType(statement)
+    print (statement," -> ", typeVariable)
